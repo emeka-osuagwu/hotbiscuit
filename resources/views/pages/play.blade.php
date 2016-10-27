d266d7d20dd4e5c0cbb6dc9a07c452a335cd6642
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-10-27 15:09:35 +0100

    update play page

--- resources/views/pages/play.blade.php
+++ resources/views/pages/play.blade.php
@@ -9,13 +9,19 @@
        <div class="test-card-section-1__content__card__image-holder">
          <div class="test-card-section-1__content__card__question-count question-count">Question {{ $number_of_played_questions + 1 }} </div>
        </div>
+
+       
+       @if( $question->question != "Choose one")
+         <div class="question-title"> {{ $question->question }}?</div>
+       @endif
+
         <div class="test-card-section-1__content__card__question-holder question-holder">
           <div class="option">
-            <h3>A: {{ $question->question_1 }}</h3>
+            <h3>A: {{ $question->option_1 }}</h3>
           </div>
           <span>OR</span>
           <div class="option">
-            <h3>B: {{ $question->question_2 }}</h3>
+            <h3>B: {{ $question->option_2 }}</h3>
           </div>
         </div>
         <div class="button-select">
@@ -26,11 +32,13 @@
             <button type="submit" class="btn option-a">A</button>
           </form>          
 
-          <form action="{{ Url('question/answer') }}" method="post">
+          <form action="{{ Url('play') }}" method="post">
             <input type="text" hidden="true" name="question_id"  value="{{ $question->id }}">
             <input type="text" hidden="true" name="answer"  value="{{ $question->option_2 }}">
+            <input type="text" hidden="true" name="player_id"  value="{{ $player_id }}">
             <button type="submit" class="btn option-b">B</button>
-          </form>
+          </form>          
+
         </div>
       </div>
   </section>
