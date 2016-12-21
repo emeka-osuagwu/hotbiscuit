8efef47a0eb5e7952976f494255e68973a8fb45f
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-12-21 15:41:13 +0100

    update

--- database/migrations/2016_10_22_015155_create_questions_table.php
+++ database/migrations/2016_10_22_015155_create_questions_table.php
@@ -13,7 +13,7 @@ class CreateQuestionsTable extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->increments('id')->nullable();
+            $table->increments('id');
             $table->string('question')->nullable();
             $table->string('option_1')->nullable();
             $table->string('option_2')->nullable();
